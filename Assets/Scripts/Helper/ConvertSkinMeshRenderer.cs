2d15117c6d33254170ed8c42cf29348500a7d589
ShadoofJumper <ShadoofJumper@gmail.com>
2020-03-05 21:01:22 +0200

    Fix bugs, add items preview meshs
    Fix mesh blend bug. Create script then convert skin mest in to standart mesh for mesh preview on location.

---
+++ Assets/Scripts/Helper/ConvertSkinMeshRenderer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConvertSkinMeshRenderer : MonoBehaviour
+{
+    
+    [ContextMenu("Convert skin to mesh")]
+    void Convert()
+    {
+        // get skin mesh renderer
+        SkinnedMeshRenderer skinMesh = GetComponent<SkinnedMeshRenderer>();
+        // create new components
+        MeshRenderer meshRenderer = gameObject.AddComponent<MeshRenderer>();
+        MeshFilter meshFilter = gameObject.AddComponent<MeshFilter>();
+
+        meshRenderer.sharedMaterials = skinMesh.sharedMaterials;
+        meshFilter.sharedMesh = skinMesh.sharedMesh;
+        // delet skinmeshrenderer
+        DestroyImmediate(skinMesh);
+        // delet ckript from obejct
+        DestroyImmediate(this);
+    }
+}
